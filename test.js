2371d8ad44bf50a55f418c110d0e88d7d624eee7
Sindre Sorhus <sindresorhus@gmail.com>
2016-06-16 14:20:28 +0200

    meta tweaks

--- test.js
+++ test.js
@@ -1,18 +1,19 @@
-'use strict';
 require('es6-set/implement');
-var assert = require('assert');
-var requireUncached = require('require-uncached');
+const test = require('ava');
+const requireUncached = require('require-uncached');
 
-it('should remove duplicates from an array', function () {
-	var arrayUniq = require('./');
-	assert.deepEqual(arrayUniq([1, 2, 2, 3, 1, 2, 4]), [1, 2, 3, 4]);
-	assert.deepEqual(arrayUniq(['a', 'a', 'b', 'a', 'c', 'a', 'd']), ['a', 'b', 'c', 'd']);
+test('removes duplicates from an array', t => {
+	const m = require('./');
+
+	t.deepEqual(m([1, 2, 2, 3, 1, 2, 4]), [1, 2, 3, 4]);
+	t.deepEqual(m(['a', 'a', 'b', 'a', 'c', 'a', 'd']), ['a', 'b', 'c', 'd']);
 });
 
-it('should remove duplicates from an array using Set', function () {
+test('removes duplicates from an array using Set', t => {
 	delete global.Set;
-	var arrayUniq = requireUncached('./');
-	assert.deepEqual(arrayUniq([1, 2, 2, 3, 1, 2, 4]), [1, 2, 3, 4]);
-	assert.deepEqual(arrayUniq(['a', 'a', 'b', 'a', 'c', 'a', 'd']), ['a', 'b', 'c', 'd']);
+	const m = requireUncached('./');
+
+	t.deepEqual(m([1, 2, 2, 3, 1, 2, 4]), [1, 2, 3, 4]);
+	t.deepEqual(m(['a', 'a', 'b', 'a', 'c', 'a', 'd']), ['a', 'b', 'c', 'd']);
 });
 
